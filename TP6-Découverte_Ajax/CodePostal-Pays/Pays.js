7ebe837f96604456b0ccedb84f1779904553a843
Hakaien <kahin54@gmail.com>
2020-11-06 11:19:30 +0100

    màj pays

--- TP6-Découverte_Ajax/CodePostal-Pays/Pays.js
+++ TP6-Découverte_Ajax/CodePostal-Pays/Pays.js
@@ -1,6 +1,7 @@
-$(window).on("load", function(){
+$(window).on("load", function () {
 
-        setTimeout(function(){
+    setTimeout(function () {
+        // appel ajax sur le fichier pays.json
         $.ajax({
             url: "pays.json",
             type: "GET",
@@ -8,23 +9,44 @@ $(window).on("load", function(){
 
             success: function (data) {
                 console.log(data);
+                // création du déroulant avec le nom des pays
+                for (let i = 0; i< data.length; i++ ) {
+                    let choixValue = data[i].alpha2;
+                    let choixText = data[i].nom_fr_fr;
+                    let deroulant = "<option value=\""+choixValue+"\">"+choixText+"</option>"
+                    $(deroulant).appendTo($("#pays"));
+                }
+
+                $("#btnAfficher").on("click", function() {
+                    let choixPays = $("#pays option:selected").text();
+                    let choixValue = $("#pays option:selected").val();
+                    console.log(choixPays);
+                    console.log(choixValue);
+
+                    let champ = "";
+                    champ+="<img src=\"./flags/"+choixValue+".png\" style='height:200px'>"
+                    champ+="<p>Code Pays : "+choixValue+" </p>"
+                    champ+="<p>Nom du Pays : "+choixPays+" </p>"
+
+                    $(champ).appendTo($("#champ"));
+
+                });
+
+                $("#btnRechercher").on("click", function () {
+                   let temp = $("#chercher").val();
+                   console.log(temp);
+                   let pays = data[40].alpha2;
+                   console.log(pays);
+                   console.log(data.length);
+                });
             },
 
             error: function () {
                 console.log("error 66");
-            } 
+            }
         })
-    },4000);
+    }, 1000);
+
     
-    var html ="";
-    html += 
-
-
-    $("#btnRechercher").on("click", function(){
-        if ($("#chercher").) {
-
-        }else {
-            console.log("erreur, mauvais pays !")
-        }
-    });
+  
 });
