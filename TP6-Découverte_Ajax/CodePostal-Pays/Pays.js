e824643f956d16831f3e7b09d053ab083752126f
Hakaien <kahin54@gmail.com>
2020-11-09 00:06:06 +0100

    midnight shit

--- TP6-Découverte_Ajax/CodePostal-Pays/Pays.js
+++ TP6-Découverte_Ajax/CodePostal-Pays/Pays.js
@@ -1,5 +1,27 @@
 $(window).on("load", function () {
 
+    $("#btn1").on("click", function () {
+
+        $("#part1").css("display", "block");
+        $("#part2").css("display", "none");
+        $("#part3").css("display", "none");
+    })
+
+
+    $("#btn2").on("click", function () {
+        $("#part2").css("display", "block");
+        $("#part1").css("display", "none");
+        $("#part3").css("display", "none");
+    });
+
+
+    $("#btn3").on("click", function () {
+        $("#part3").css("display", "block");
+        $("#part1").css("display", "none");
+        $("#part2").css("display", "none");
+    });
+
+
     setTimeout(function () {
         // appel ajax sur le fichier pays.json
         $.ajax({
@@ -10,34 +32,34 @@ $(window).on("load", function () {
             success: function (data) {
                 console.log(data);
                 // création du déroulant avec le nom des pays
-                for (let i = 0; i< data.length; i++ ) {
+                for (let i = 0; i < data.length; i++) {
                     let choixValue = data[i].alpha2;
                     let choixText = data[i].nom_fr_fr;
-                    let deroulant = "<option value=\""+choixValue+"\">"+choixText+"</option>"
+                    let deroulant = "<option value=\"" + choixValue + "\">" + choixText + "</option>"
                     $(deroulant).appendTo($("#pays"));
                 }
 
-                $("#btnAfficher").on("click", function() {
+                $("#btnAfficher").on("click", function () {
                     let choixPays = $("#pays option:selected").text();
                     let choixValue = $("#pays option:selected").val();
                     console.log(choixPays);
                     console.log(choixValue);
 
                     let champ = "";
-                    champ+="<img src=\"./flags/"+choixValue+".png\" style='height:200px'>"
-                    champ+="<p>Code Pays : "+choixValue+" </p>"
-                    champ+="<p>Nom du Pays : "+choixPays+" </p>"
+                    champ += "<img src=\"./flags/" + choixValue + ".png\" style='height:200px'>"
+                    champ += "<p>Code Pays : " + choixValue + " </p>"
+                    champ += "<p>Nom du Pays : " + choixPays + " </p>"
 
-                    $("#champ").html(champ);
+                    $("#champ1").html(champ);
 
                 });
 
                 $("#btnRechercher").on("click", function () {
-                   let temp = $("#chercher").val();
-                   console.log(temp);
-                   let pays = data[40].alpha2;
-                   console.log(pays);
-                   console.log(data.length);
+                    let temp = $("#chercher").val();
+                    console.log(temp);
+                    let pays = data[40].alpha2;
+                    console.log(pays);
+                    console.log(data.length);
                 });
             },
 
@@ -47,6 +69,6 @@ $(window).on("load", function () {
         })
     }, 1000);
 
-    
-  
+
+
 });
